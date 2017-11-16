350b8a9aec501c4d530cd7df5e7715b8ded525f6
Brandon Lin <blin00@gmail.com>
2017-11-16 02:08:13 -0800

    fish ssh-agent; cygwin symlinks; python

--- config.fish
+++ config.fish
@@ -8,6 +8,7 @@ end
 set -x EDITOR vim
 set -x VISUAL vim
 set -x PYTHONSTARTUP $HOME/.pythonrc
+set -x CYGWIN winsymlinks:native
 
 set fish_greeting
 
@@ -17,4 +18,8 @@ function ls --description 'list directory contents'
             set param $param -h --indicator-style=classify
         end
         command ls $param $argv
+end
+
+if begin; test -z (command pgrep ssh-agent); end
+    eval (command ssh-agent -c | sed 's/^setenv/set -Ux/')
 end

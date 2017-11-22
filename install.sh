bf8ec6d6db5b909d343db806044a01a1b9e6b9e5
Brandon Lin <blin00@gmail.com>
2017-11-22 00:56:56 -0800

    add basic .gdbinit; force unix line endings on windows

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ export CYGWIN=winsymlinks:native
 mkdir -p $HOME/.config/fish
 
 ln -sfv $HOME/dotfiles/config.fish $HOME/.config/fish
+ln -sfv $HOME/dotfiles/.gdbinit $HOME
 ln -sfv $HOME/dotfiles/.gitconfig $HOME
 ln -sfv $HOME/dotfiles/.pythonrc $HOME
 ln -sfv $HOME/dotfiles/.radare2rc $HOME

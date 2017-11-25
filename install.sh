534c4543f2c442f6da62893232bb03d9fa7fb876
Brandon Lin <blin00@gmail.com>
2017-11-25 15:39:22 -0800

    add tmux config

--- install.sh
+++ install.sh
@@ -9,4 +9,5 @@ ln -sfv $HOME/dotfiles/.gdbinit $HOME
 ln -sfv $HOME/dotfiles/.gitconfig $HOME
 ln -sfv $HOME/dotfiles/.pythonrc $HOME
 ln -sfv $HOME/dotfiles/.radare2rc $HOME
+ln -sfv $HOME/dotfiles/.tmux.conf $HOME
 ln -sfv $HOME/dotfiles/.vimrc $HOME

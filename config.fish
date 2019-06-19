a0555889988bff390791faa53c5ea4b9c273b7df
Brandon Lin <blin00@gmail.com>
2019-06-19 16:32:36 -0700

    add explicit fish colors; macOS related stuff

--- config.fish
+++ config.fish
@@ -1,9 +1,34 @@
+set -l os (uname)
+
 set fish_greeting
 
-set -l add_to_path $HOME/opt/cross/bin $HOME/.cargo/bin $HOME/.local/bin $HOME/bin /usr/local/go/bin /usr/local/cuda/bin
-for entry in $add_to_path
-    if test -d $entry
-    and not contains $entry $PATH
+set fish_color_autosuggestion 555 brblack
+set fish_color_cancel -r
+set fish_color_command --bold
+set fish_color_comment red
+set fish_color_cwd green
+set fish_color_cwd_root red
+set fish_color_end brmagenta
+set fish_color_error brred
+set fish_color_escape bryellow --bold
+set fish_color_history_current --bold
+set fish_color_host normal
+set fish_color_match --background=brblue
+set fish_color_normal normal
+set fish_color_operator bryellow
+set fish_color_param cyan
+set fish_color_quote yellow
+set fish_color_redirection brblue
+set fish_color_search_match bryellow --background=brblack
+set fish_color_selection white --bold --background=brblack
+set fish_color_status red
+set fish_color_user brgreen
+set fish_color_valid_path --underline
+
+set -l prepend_to_path $HOME/opt/cross/bin $HOME/.cargo/bin $HOME/.local/bin $HOME/bin /usr/local/go/bin /usr/local/sbin /usr/local/cuda/bin $HOME/Library/Python/3.7/bin
+
+for entry in $prepend_to_path
+    if test -d $entry; and not contains $entry $PATH
         set -gx PATH $entry $PATH
     end
 end
@@ -35,17 +60,35 @@ alias gcm="git commit -m"
 alias gd="git diff"
 alias gdc="git diff --cached"
 alias gl="git log"
+alias glo="git log --oneline"
 alias glp="git log -p"
 alias gs="git status"
 
 alias pacman="pacman --color auto"
 
-function ls --description 'list directory contents'
-    set -l param --color=auto
-    if isatty stdout
-        set param $param -h --indicator-style=classify
+if test $os = 'Darwin'
+    alias shred="gshred"
+    alias base64="gbase64"
+
+    alias md5sum="gmd5sum"
+    alias sha1sum="gsha1sum"
+    alias sha256sum="gsha256sum"
+
+    function ls --description 'List contents of directory'
+        set -l param -G
+        if isatty stdout
+            set param $param -h -F
+        end
+        command ls $param $argv
     end
-    command ls $param $argv
+else
+    function ls --description 'List contents of directory'
+        set -l param --color=auto
+        if isatty stdout
+            set param $param -h --indicator-style=classify
+        end
+        command ls $param $argv
+    end
 end
 
 if test -f $HOME/.ssh/.agent

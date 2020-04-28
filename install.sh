885ab41d6bbfc3ae9d46bd1d047960e753e3a35a
Brandon Lin <blin00@gmail.com>
2020-04-27 23:09:24 -0700

    add kitty config

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+set -eu
+
 export CYGWIN=winsymlinks:native
 
 mkdir -p $HOME/.config/fish
+mkdir -p $HOME/.config/kitty
 
 ln -sfv $HOME/dotfiles/config.fish $HOME/.config/fish
+ln -sfv $HOME/dotfiles/kitty.conf $HOME/.config/kitty
 ln -sfv $HOME/dotfiles/.gdbinit $HOME
 ln -sfv $HOME/dotfiles/.gitconfig $HOME
 ln -sfv $HOME/dotfiles/.npmrc $HOME

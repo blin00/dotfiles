16a736290b4d58bb4a465ab8140c5e5024f717f9
Brandon Lin <blin00@gmail.com>
2020-05-04 20:40:09 -0700

    fix fish 2.4, better private prompt

--- config.fish
+++ config.fish
@@ -25,31 +25,16 @@ set fish_color_status red
 set fish_color_user brgreen
 set fish_color_valid_path --underline
 
-function fish_prompt --description "Write out the prompt"
-    set -l color_cwd
-    set -l suffix
-    switch "$USER"
-        case root toor
-            if set -q fish_color_cwd_root
-                set color_cwd $fish_color_cwd_root
-            else
-                set color_cwd $fish_color_cwd
-            end
-            set suffix '#'
-        case '*'
-            set color_cwd $fish_color_cwd
-            set suffix '>'
-    end
-
-    echo -n -s "$USER" @ (prompt_hostname) ' '
+function fish_right_prompt
     if set -q fish_private_mode
-        echo -n -s '[private] '
+        set_color purple
+        echo -n '[private]'
+        set_color normal
     end
-    echo -n -s (set_color $color_cwd) (prompt_pwd) (set_color normal) "$suffix "
 end
 
 function fish_title
-    echo (status current-command) (prompt_pwd)
+    echo $_ (prompt_pwd)
 end
 
 set -l prepend_to_path $HOME/opt/cross/bin $HOME/.cargo/bin $HOME/.local/bin $HOME/bin $HOME/android/platform-tools /usr/local/go/bin /usr/local/sbin /usr/local/cuda/bin $HOME/Library/Python/3.7/bin

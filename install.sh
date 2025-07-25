d917c55e0b7bd7883f46bf1830c2ce55f4898013
Brandon Lin <blin00@gmail.com>
2025-07-25 02:29:15 -0700

    add ghostty config, stop linking .gdbinit

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ export CYGWIN=winsymlinks:native
 
 mkdir -p "$HOME"/.config/fish
 mkdir -p "$HOME"/.config/kitty
-
+mkdir -p "$HOME"/.config/ghostty
 
 if [ "$OS" = "Darwin" ]; then
     ln -sfv "$HOME"/dotfiles/kitty.macos.conf "$HOME"/.config/kitty/kitty.conf
@@ -16,8 +16,9 @@ else
     ln -sfv "$HOME"/dotfiles/kitty.conf "$HOME"/.config/kitty/kitty.conf
 fi
 
+ln -sfv "$HOME"/dotfiles/ghostty.conf "$HOME"/.config/ghostty/config
 ln -sfv "$HOME"/dotfiles/config.fish "$HOME"/.config/fish
-ln -sfv "$HOME"/dotfiles/.gdbinit "$HOME"
+#ln -sfv "$HOME"/dotfiles/.gdbinit "$HOME"
 ln -sfv "$HOME"/dotfiles/.gitconfig "$HOME"
 ln -sfv "$HOME"/dotfiles/.npmrc "$HOME"
 ln -sfv "$HOME"/dotfiles/.pythonrc "$HOME"
